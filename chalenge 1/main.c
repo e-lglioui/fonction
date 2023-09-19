19a3f7c4adb482e8aad6e15e02a4172f140e9db1
el glioui <elgliouiyoucode@gmail.com>
2023-09-19 11:25:22 +0200

    challenge 1

---
+++ chalenge 1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+void bonjour(){
+    printf("   YOU CODE   ");
+}
+int main()
+{
+    printf("***********challenge 1************\n");
+    bonjour();
+
+    return 0;
+}
